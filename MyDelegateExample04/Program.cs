fc9ac89cb9e6ba3fe821de1babe408b3d658df7d
Avinash M <avinashm201993@gmail.com>
2018-08-29 18:26:36 -0500

    Implemented Delegate

--- MyDelegateExample04/Program.cs
+++ MyDelegateExample04/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace MyDelegateExample04
 {
+    public delegate int MyDelegate(int firstNumber, int secondNumber);
+
     class Program
     {
+        static int x = 30;
+
+        static int Add(int a, int b)
+        {
+            x = a + b;
+            return x;
+        }
+        static int Sub(int a, int b)
+        {
+            x = a - b;
+            return x;
+        }
+
         static void Main(string[] args)
         {
+
+            MyDelegate md = Add;
+            md += Sub;
+           
+            Console.WriteLine(md(10, x)); // x = 10 + 30; x = 10 - 30
+            Console.WriteLine(x);
+
+            md -= Add;
+            Console.WriteLine(md(40, x)); //x = 40 -(-20)
+            Console.WriteLine(x);
         }
     }
 }
